b7d518057837fdc94ffc76d934f896ea688a8079
Neha Rathi <neharrathi@gmail.com>
2013-10-26 20:57:08 -0400

    Added flag handling to main.

--- src/Main.java
+++ src/Main.java
@@ -2,19 +2,33 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Map;
 
-
 public class Main {
 
 	public static void main(String[] args) throws IOException {
-		if (args.length == 0) {
-			System.out.println("Give config file name");
+		try {
+			FacilityManager facilityManager;
+			if (args.length == 2 && (args[0].equals("-m") || args[0].equals("-s"))) {
+				if (args[0].equals("-m")) {
+					// FacilityManager should behaves as the master.
+					Config config = new Config(new File(args[1]));
+					facilityManager = new FacilityManager();
+				} else {
+					// FacilityManager should behave as a slave.
+					String masterIp = args[1];
+					facilityManager = new FacilityManager();
+				}
+			} else {
+				System.out.println("Usage: FacilityManager -m <configFile> for masters and " +
+						"FacilityManager -s <hostname> for participants");
+				return;
+			}
+
+			facilityManager.run();
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.exit(0);
 		}
-		File cFile = new File(args[0]);
-		Config config = new Config(cFile);
-		
-		FacilityManager manager = new FacilityManager();
-		manager.run();
-		
+
 		ProcessBuilder pb = new ProcessBuilder("myshellScript.sh", "myArg1", "myArg2");
 		Map<String, String> env = pb.environment();
 		env.put("VAR1", "myValue");
