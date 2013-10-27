3a6733343cf11740dda6e61937236839749e7dd5
Neha Rathi <neharrathi@gmail.com>
2013-10-26 20:04:28 -0400

    Added config parsing.

--- src/Config.java
+++ src/Config.java
@@ -1,22 +1,68 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.io.Serializable;
 
 public class Config implements Serializable {
-	
-	private String[] participantsIp;
+
+	private static final long serialVersionUID = 1525640055670485006L;
+
 	private String masterIp;
-	
-	private int FSport;
-	private int MRport;
-	
-	private int replicationFactor;
-	private int fileBlockSize;
-	
+	private String[] participantIps;
+
+	private int fsPort;
+	private int mrPort;
+
 	private int maxMapsPerHost;
 	private int maxReducesPerHost;
-	
-	
+
+	private int replicationFactor;
+	private int blockSize;
 
 	public Config() {
-		
+
+	}
+
+	public Config(File f) {
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader(f));
+			String line = null;
+			while ((line = reader.readLine()) != null) {
+				if (!(line.startsWith("#") || line.isEmpty())) {
+					if (line.startsWith("MASTER_IP")) {
+						masterIp = line.substring(line.indexOf('=') + 1);
+						System.out.println("masterIp: " + masterIp);
+					} else if (line.startsWith("PARTICIPANT_IPS")) {
+						participantIps = line.substring(line.indexOf('=') + 1).split(",");
+						System.out.println("participantIps: " + participantIps);
+					} else if (line.startsWith("FS_PORT")) {
+						fsPort = Integer.parseInt(line.substring(line.indexOf('=') + 1));
+						System.out.println("fsPort: " + fsPort);
+					} else if (line.startsWith("MR_PORT")) {
+						mrPort = Integer.parseInt(line.substring(line.indexOf('=') + 1));
+						System.out.println("mrPort: " + mrPort);
+					} else if (line.startsWith("MAX_MAPS_PER_HOST")) {
+						maxMapsPerHost = Integer.parseInt(line.substring(line.indexOf('=') + 1));
+						System.out.println("maxMapsPerHost: " + maxMapsPerHost);
+					} else if (line.startsWith("MAX_REDUCES_PER_HOST")) {
+						maxReducesPerHost = Integer.parseInt(line.substring(line.indexOf('=') + 1));
+						System.out.println("maxReducesPerHost: " + maxReducesPerHost);
+					} else if (line.startsWith("REPLICATION_FACTOR")) {
+						replicationFactor = Integer.parseInt(line.substring(line.indexOf('=') + 1));
+						System.out.println("replicationFactor: " + replicationFactor);
+					} else if (line.startsWith("BLOCK_SIZE")) {
+						blockSize = Integer.parseInt(line.substring(line.indexOf('=') + 1));
+						System.out.println("blockSize: " + blockSize);
+					}
+				}
+			}
+			reader.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
