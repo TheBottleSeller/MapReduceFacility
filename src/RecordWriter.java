3682b743c68b18fa7388a35a5b4304dc3c6ed940
Neha Rathi <neharrathi@gmail.com>
2013-11-11 16:16:27 -0500

    Made record reader and record writer.

---
+++ src/RecordWriter.java
@@ -0,0 +1,27 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
+import java.util.List;
+
+public class RecordWriter {
+	PrintWriter writer;
+
+	public RecordWriter(File outFile) throws FileNotFoundException {
+		this.writer = new PrintWriter(new FileOutputStream(outFile));
+	}
+
+	public void writeKeyValues(String key, String value) {
+		writer.println(key);
+		writer.println(value);
+	}
+
+	public void writeKeyMultiValues(String key, List<String> values) {
+		writer.println(key);
+		writer.println(values.size());
+		for (String value : values) {
+			writer.println(value);
+		}
+	}
+
+}
