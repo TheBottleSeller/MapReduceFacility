628eaaaf870a0603d63d7ecff57e298e036ce2a3
Neha Rathi <neharrathi@gmail.com>
2013-11-12 15:13:06 -0500

    Removed unused imports.

--- src/JobScheduler.java
+++ src/JobScheduler.java
@@ -1,10 +1,8 @@
 import java.io.FileNotFoundException;
 import java.rmi.RemoteException;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.atomic.AtomicInteger;
--- src/MapReduceJob.java
+++ src/MapReduceJob.java
@@ -1,5 +1,3 @@
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
